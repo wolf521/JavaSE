d960d2c2669acf8b8c2347936a0940c46464f13c
cuizhixiang <cuizhixiang_java@163.com>
2018-03-17 23:12:13 +0800

    多态

---
+++ JavaSE/src/polymorphic/Polymorphic.java
@@ -0,0 +1,20 @@
+package polymorphic;
+
+public abstract class Polymorphic {
+	public abstract void run();
+	
+	public void eat() {
+		
+	}
+	
+	public void eat(String foodNname) {}
+}
+class Pol extends Polymorphic{
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
