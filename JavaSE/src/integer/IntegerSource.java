b4a2595e95670ccaf20c81ddf81726a9639e41b9
cuizhixiang <cuizhixiang_java@163.com>
2018-03-22 21:52:35 +0800

    位运算

--- JavaSE/src/integer/IntegerSource.java
+++ JavaSE/src/integer/IntegerSource.java
@@ -5,22 +5,32 @@ public class IntegerSource {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Integer i1 = 1;
-		Integer i2 = 1;
-		Integer i3 = 128;
-		System.out.println(i1 == i2);
-		System.out.println(i1 == i3);
-		
-		Integer s1 = new Integer(12);
-		Integer s2 = new Integer(12);
-		System.out.println(s1 == s2);
-		System.out.println(s1.equals(s2));
-		test(s1,s2);
-		System.out.println(s1+""+s2);
+//		Integer i1 = 1;
+//		Integer i2 = 1;
+//		Integer i3 = 128;
+//		System.out.println(i1 == i2);
+//		System.out.println(i1 == i3);
+//		
+//		Integer s1 = new Integer(12);
+//		Integer s2 = new Integer(12);
+//		System.out.println(s1 == s2);
+//		System.out.println(s1.equals(s2));
+//		test(s1,s2);
+//		System.out.println(s1+""+s2);
+		bitOperation();
 	}
 	
 	private static void test(Integer s1,Integer s2) {
 		s1 = 222;
 		s2 = 444;
+	}
+	/**
+	 * Î»ÔËËã
+	 * 
+	 */
+	private static void bitOperation() {
+		int i = 2;
+		i = i >>> 2;
+		System.out.println(i);
 	}
 }
